8454ce895814f2c89395ebec5089ee68bced6a7d
dzbeda <44648295+dzbeda@users.noreply.github.com>
2021-12-25 18:54:57 +0200

    Update exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+count=0
+if [ $HOSTNAME == server1 ]
+then
+  echo "start copy from $HOSTNAME server to server2 "
+  for i in "${@:1:$# - 1}"
+  do
+    echo "Copy file" $i to "${@: -1}" "on remote server"
+    scp $i server2:"${@: -1}"
+    count=$((count + $(ls -la $i | awk '{print $5}')))
+  done
+else
+  echo "start copy from $HOSTNAME server to server1 "
+  for i in "${@:1:$# - 1}"
+  do
+    echo "Copy file" $i to "${@: -1}" "on remote server"
+    scp $i server1:"${@: -1}"
+    count=$((count + $(ls -la $i | awk '{print $5}')))
+  done
+fi
+echo $count
